f288bbd8aa547225b65d687479b19ec395d547b2
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-07-23 14:10:44 +0300

    feat: implement teacher analytics dashboard with revenue and sales visualization

--- src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,18 @@
-export default function TeacherAnalyticsPage() {
+import { getAnalytics } from "@/actions/get-analytics";
+import { auth } from "@clerk/nextjs/server";
+import { DataCard } from "./_components/DataCard";
+import { Chart } from "./_components/Chart";
+
+export default async function TeacherAnalyticsPage() {
+  const { userId } = await auth();
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId!);
   return (
-    <div>
-      <h1>TeacherAnalyticsPage</h1>
+    <div className="p-6">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat/>
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+      <Chart data={data} />
     </div>
   );
 }
